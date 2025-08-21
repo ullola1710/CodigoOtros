c43a8956e0178def7672c771dadfd2f6cad6b056
ullola_1710 <jadeullola@icloud.com>
2025-08-21 12:04:37 -0600

    Se agregan cambios pt 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,35 @@
-const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const baseEndpoint = 'https://api.github.com'; // Consumo de API
+const usersEndpoint = `${baseEndpoint}/users`; // Endpoint de la API de acuerdo al usuario
+
+// Variables que vienen del index.html
+// Cambio de selectores, son clases
+const $n = document.querySelector('.name'); // + [.]
+const $b = document.querySelector('.blog'); // - [#] + [.] 
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+// Se creaba conflicto con "await" - se cambia función a que sea asincrona
+// Como se llama a la API, se ocupa "await" por si llega a tardar
+async function displayUser(username) {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+
+  try {
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    // Se agrega constante data 
+    const data = await response.json();
+    console.log(data);
+    // Deben ser backticks, mas no comillas sencillas
+    $n.textContent = `${data.name}`; // + $n 
+    $b.textContent = `${data.blog}`;
+    $l.textContent = `${data.location}`;
+  } catch (error) {
+    throw new Error("Error al llamar API");
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski').catch(handleError); // Nombre de usuario displayUser('stolinski')
